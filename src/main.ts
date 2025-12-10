ee6ed55aa0fe9de2a83a81d9ed004c30e405f8fd
carlosrb <carlos.romero.breto@gmail.com>
2025-12-10 18:58:43 +0100

    changes in preparation

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { Root } from './root/root.component';
 import { rootConfig } from './root/root.config';
 
-bootstrapApplication(Root, rootConfig).catch((err) => console.error(err));
+bootstrapApplication(Root, rootConfig).catch((err) => {
+  console.error(err);
+});
