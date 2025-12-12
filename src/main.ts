2ca0981d761f2c2fa57b454ba2473125eff35761
carlosrb <carlos.romero.breto@gmail.com>
2025-12-12 06:41:20 +0100

    add router package

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { RootComponent } from './root/root.component';
 
 const applicationConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners()],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(
+      [
+        // TODO
+      ],
+      withEnabledBlockingInitialNavigation()
+    ),
+  ],
 };
 
 bootstrapApplication(RootComponent, applicationConfig).catch((err) => {
